8f6347e784bf49be42078b69643be726c00b763f
Amit Saha <65004568+poseidon9928@users.noreply.github.com>
2020-12-19 10:38:50 +0600

    Level addition and bugfix

--- TryFrog1/src/p4_group_8_repo/Main.java
+++ TryFrog1/src/p4_group_8_repo/Main.java
@@ -39,7 +39,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
-/** 
+/**
 * setting gameplay menu
 */
 		MyStage main_scene = new MyStage();
